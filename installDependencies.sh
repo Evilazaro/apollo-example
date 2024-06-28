bb1613c671bd485ed43ed1e13f78785fd1cfb5d0
Evilazaro <evilazaro@gmail.com>
2024-06-28 15:25:22 -0500

    Authentication

---
+++ installDependencies.sh
@@ -0,0 +1,6 @@
+# bin/bash
+
+# bash script to install dependencies for the project
+npm install @as-integrations/azure-functions @apollo/server graphql @azure/functions azure-functions-core-tools@4
+npm install @azure/msal-node 
+npm install dotenv
